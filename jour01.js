75064cc0bf575cf0b0f320e703521fc9443713cc
Amarsalah-31 <amarnawal22@gmail.com>
2021-06-14 17:35:46 +0200

    exercice1

---
+++ jour01.js
@@ -0,0 +1,13 @@
+console.log("Hello World !");
+var test ="my name is amar";
+console.log("test");
+var name ='amar'+' ' + 'nice to meet you';
+console.log(name);
+var testLength = "I'm very long !";
+console.log(testLength.length);
+var foot ="croissant is meh";
+console.log(foot.replace("meh","so good"));
+function titleCase(str){
+
+console.log(str.tolOwerCase())
+}
